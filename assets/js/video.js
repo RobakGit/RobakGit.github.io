3568299caf01edd3243de5418290753ba32b54cd
RobakGit <68401907+RobakGit@users.noreply.github.com>
2021-05-07 21:29:57 +0200

    Add files via upload

--- assets/js/video.js
+++ assets/js/video.js
@@ -4,8 +4,14 @@ var btn = document.getElementById('play-btn');
 function playVideo() {
     btn.style.display = 'none';
     vid.play();
+
 }
 
-document.getElementById("video").addEventListener('pause', () => {
+document.getElementById("video").addEventListener('click', () => {
+    vid.pause();
+    btn.style.display = 'block';
+});
+
+document.getElementById("video").addEventListener('ended', () => {
     btn.style.display = 'block';
 });
